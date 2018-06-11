c48c79076db405a09e3816a0836bbb1b023dfe9a
Quico Spaen <quico@spaen.nl>
2018-06-11 13:12:43 -0400

    Implement segmentation and post-processing

---
+++ src/hnccorr/segmentation.py
@@ -0,0 +1,100 @@
+import networkx as nx
+import numpy as np
+from scipy.ndimage.morphology import binary_fill_holes
+from itertools import product
+
+
+class Segmentations(object):
+    def __init__(self, segmentations, min_size, max_size, pref_size):
+        self._segmentations = segmentations
+        self._min_size = min_size
+        self._max_size = max_size
+        self._pref_size = pref_size
+
+    def clean(self):
+        """Keep largest component and fill holes"""
+        for s in self._segmentations:
+            s.clean()
+
+    def _filter(self):
+        for s in self._segmentations:
+            print(len(s.selection))
+        return [
+            s for s in self._segmentations
+            if self._min_size <= len(s.selection) <= self._max_size
+        ]
+
+    def select(self):
+        """Select best candidate segmentation"""
+        candidates = self._filter()
+        if len(candidates) == 0:
+            return None
+        size = np.array([len(c.selection) for c in candidates])
+        best_index = np.argmin(np.abs(np.sqrt(size) - np.sqrt(self._pref_size)))
+
+        return candidates[best_index]
+
+
+
+
+
+
+class Segmentation(object):
+    def __init__(self, patch, selection, weight):
+        self._patch = patch
+        self.selection = set(selection)
+        self.weight = weight
+
+    def _fill_holes(self):
+        shape = self._patch.shape
+        mask = np.full(shape, False, dtype=np.bool)
+
+        indices = list(zip(*self.selection))
+        mask[indices] = True
+
+        filled_mask = binary_fill_holes(mask)
+
+        index_arrays = [a.tolist() for a in np.where(filled_mask)]
+        self.selection = set(zip(*index_arrays))
+
+    def _select_largest_component(self):
+        shape = self._patch.shape
+        n = np.product(shape)
+        zero_tuple = (0, ) * len(shape)
+
+        def tuple_add(x, y):
+            return tuple(i + j for i, j in zip(x, y))
+
+        max_tuple = tuple_add(shape, (-1, ) * len(shape))
+
+        def neighborhood(num_dims):
+            for dim, change in product(range(num_dims), (-1, 1)):
+                shift = [0, ] * num_dims
+                shift[dim] = change
+                yield shift
+
+        neighbors = tuple(neighborhood(len(shape)))
+
+        G = nx.Graph()
+        G.add_nodes_from(self.selection)
+
+        for index, shift in product(self.selection, neighbors):
+            neighbor = tuple_add(index, shift)
+            if zero_tuple <= neighbor <= max_tuple and neighbor in G:
+                G.add_edge(index, neighbor)
+
+        components = list(nx.connected_components(G))
+
+        overlap = [
+            len(c.intersection(self._patch.positive_seeds))
+            for c in components
+        ]
+
+        best_component = components[np.argmax(overlap)]
+
+        self.selection = best_component
+
+    def clean(self):
+        """Remove left over points / fill holes"""
+        self._select_largest_component()
+        self._fill_holes()
---
+++ tests/test_segmentation.py
@@ -0,0 +1,47 @@
+import pytest
+
+
+@pytest.fixture
+def P():
+    class Mock_Patch(object):
+        def __init__(self):
+            self.shape = (5, 5)
+            self.positive_seeds = set([(1, 0)])
+
+    return Mock_Patch()
+
+
+@pytest.fixture
+def S1(P):
+    from hnccorr.segmentation import Segmentation
+
+    return Segmentation(P, set([(0, 0), (1, 0), (2, 0), (0, 1), (2, 1), (0, 2), (1, 2), (2, 2)]), 0.5)
+
+
+@pytest.fixture
+def S2(P):
+    from hnccorr.segmentation import Segmentation
+
+    return Segmentation(P, set([(1, 0), ]), 0.5)
+
+
+@pytest.fixture
+def SS(S1, S2):
+    from hnccorr.segmentation import Segmentations
+
+    return Segmentations([S1, S2], 2, 10, 3)
+
+
+def test_segmentation_clean(S1):
+    S1.clean()
+    assert S1.selection == set([(0, 0), (1, 0), (2, 0), (0, 1), (1, 1), (2, 1), (0, 2), (1, 2), (2, 2)])
+
+
+def test_segmentations_clean(SS, S1):
+    SS.clean()
+    S1.clean()
+    assert SS.select().selection == S1.selection
+
+
+def test_best_segmentation(SS, S1):
+    assert SS.select().weight == S1.weight
