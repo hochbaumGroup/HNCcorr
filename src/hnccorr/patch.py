94f96f52debeacfccc9cacfa56d0c969b5cf677f
Quico Spaen <quico@spaen.nl>
2018-06-28 13:36:15 +0200

    Add boundary shifting to patch when seed is close to boundary

--- src/hnccorr/patch.py
+++ src/hnccorr/patch.py
@@ -4,22 +4,19 @@ from hnccorr.utils import add_offset_coordinates, add_offset_set_coordinates
 class Patch(object):
     """Subregion of movie with seeds to evaluate for cell presence
     """
+
     def __init__(self, movie, center_seed, window_size, positive_seeds):
         if window_size % 2 == 0:
             raise ValueError("window_size (%d) should be an odd number.")
-        else:
-            half_width = int((window_size - 1) / 2)
 
         self._num_dimensions = len(movie.pixel_size)
         self._window_size = window_size
         self._movie = movie
-        self.pixel_size = (window_size, ) * self._num_dimensions
+        self.pixel_size = (window_size,) * self._num_dimensions
         self.num_frames = movie.num_frames
-
         self._center_seed = center_seed
-        self._topleft_coordinates = add_offset_coordinates(
-            center_seed, (-half_width,) * self._num_dimensions
-        )
+
+        self._topleft_coordinates = self._patch_boundaries_from_center()
 
         offset = list(-x for x in self._topleft_coordinates)
         self.positive_seeds = add_offset_set_coordinates(
@@ -27,6 +24,34 @@ class Patch(object):
         )
 
         self._data = self._movie[self._movie_indices()]
+
+    def _patch_boundaries_from_center(self):
+        half_width = int((self._window_size - 1) / 2)
+
+        topleft_coordinates = add_offset_coordinates(
+            self._center_seed, (-half_width,) * self._num_dimensions
+        )
+        # shift left such that top left corner exists
+        topleft_coordinates = list(max(x, 0) for x in topleft_coordinates)
+
+        # bottomright corners (python-style index so not included)
+        bottomright_coordinates = add_offset_coordinates(
+            topleft_coordinates, (self._window_size,) * self._num_dimensions
+        )
+        # shift right such that bottom right corner exists
+        bottomright_coordinates = list(
+            min(x, max_value)
+            for x, max_value in zip(
+                bottomright_coordinates, self._movie.pixel_size
+            )
+        )
+
+        topleft_coordinates = add_offset_coordinates(
+            bottomright_coordinates,
+            (-self._window_size,) * self._num_dimensions,
+        )
+
+        return topleft_coordinates
 
     def _movie_indices(self):
         """Compute movie index range of patch"""
--- tests/test_patch.py
+++ tests/test_patch.py
@@ -9,9 +9,36 @@ def P(MM):
     return Patch(MM, (5, ), 7, set([(4,), (5,), (6, )]))
 
 
+@pytest.fixture
+def P_boundary_max(MM):
+    from hnccorr.patch import Patch
+
+    return Patch(MM, (9, ), 7, set([(8,), (9, )]))
+
+
+@pytest.fixture
+def P_boundary_min(MM):
+    from hnccorr.patch import Patch
+    return Patch(MM, (1, ), 7, set([(0,), (1,), (2, )]))
+
+
 def test_patch(P, MM):
     assert P.pixel_size == (7, )
     assert P.num_frames == 3
     assert P.positive_seeds == set([(2, ), (3, ), (4,)])
 
     np.testing.assert_equal(P[:], MM[:, 2:9])
+
+
+def test_patch_boundary(P_boundary_min, P_boundary_max, MM):
+    assert P_boundary_min.pixel_size == (7, )
+    assert P_boundary_min.num_frames == 3
+    assert P_boundary_min.positive_seeds == set([(0, ), (1, ), (2,)])
+
+    np.testing.assert_equal(P_boundary_min[:], MM[:, :7])
+
+    assert P_boundary_max.pixel_size == (7, )
+    assert P_boundary_max.num_frames == 3
+    assert P_boundary_max.positive_seeds == set([(5,), (6,)])
+
+    np.testing.assert_equal(P_boundary_max[:], MM[:, 3:])
