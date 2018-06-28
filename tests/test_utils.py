0819d19835f7cc9bedeec98e5474765924ca874a
Quico Spaen <quico@spaen.nl>
2018-06-28 15:40:22 +0200

    Add util tests

---
+++ tests/test_utils.py
@@ -0,0 +1,13 @@
+def test_add_offset():
+    from hnccorr.utils import add_offset_coordinates
+
+    assert add_offset_coordinates((1, 2), (3, 4)) == (4, 6)
+
+
+def test_add_set_offset():
+    from hnccorr.utils import add_offset_set_coordinates
+
+    assert add_offset_set_coordinates({(0, 1), (1, 1)}, (2, 2)) == {
+        (2, 3),
+        (3, 3),
+    }
