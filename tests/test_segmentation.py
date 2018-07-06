326756a34d738e416b63fecdd5c14ac44456d690
Quico Spaen <quico@spaen.nl>
2018-07-06 15:02:25 +0200

    Add test for segmentation attributes

--- tests/test_segmentation.py
+++ tests/test_segmentation.py
@@ -1,11 +1,24 @@
 import pytest
 
 
-def test_segmentation(S1):
-    assert S1.weight == 10
+def test_weight(S1):
+    assert S1.weight == 0.5
 
 
-def test_segmentation_clean(S1):
+def test_selection(S1):
+    assert S1.selection == {
+        (0, 0),
+        (1, 0),
+        (2, 0),
+        (0, 1),
+        (2, 1),
+        (0, 2),
+        (1, 2),
+        (2, 2),
+    }
+
+
+def test_clean(S1):
     S1.clean()
     assert S1.selection == {
         (0, 0),
