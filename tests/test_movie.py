681df744eab48ad88774869f070ec2ca45e40a2b
Quico Spaen <quico@spaen.nl>
2018-07-06 14:42:25 +0200

    Rewrite movie tests

--- tests/test_movie.py
+++ tests/test_movie.py
@@ -3,9 +3,8 @@ import os
 import numpy as np
 
 TEST_DATA_DIR = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)),
-    'test_data',
-    )
+    os.path.dirname(os.path.realpath(__file__)), "test_data"
+)
 
 
 @pytest.fixture
@@ -18,16 +17,30 @@ def M():
     """
     from hnccorr.movie import Movie
 
-    return Movie('Simple',
-                 image_dir=str(os.path.join(TEST_DATA_DIR, 'simple_movie')),
-                 num_images=3)
+    return Movie(
+        "Simple",
+        image_dir=str(os.path.join(TEST_DATA_DIR, "simple_movie")),
+        num_images=3,
+    )
 
 
-def test_movie_init(M):
-    assert M.name == 'Simple'
+def test_name(M):
+    assert M.name == "Simple"
+
+
+def test_num_frames(M):
     assert M.num_frames == 3
+
+
+def test_num_pixels(M):
     assert M.num_pixels == 50
+
+
+def test_data_size(M):
     assert M.data_size == (3, 5, 10)
+
+
+def test_pixel_size(M):
     assert M.pixel_size == (5, 10)
 
 
@@ -38,5 +51,5 @@ def test_is_valid_pixel_index(M):
 
 
 def test_movie_get_item(M):
-    M[0, 0, 0] == 0.0
+    assert M[0, 0, 0] == 1.0
     np.testing.assert_allclose(M[2, :, :], np.ones((5, 10)) * 3)
