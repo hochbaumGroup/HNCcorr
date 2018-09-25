61943573d737d5644f56d65321fd21d0cf383bb3
Quico Spaen <quico@spaen.nl>
2018-09-24 21:41:09 -0700

    Add support for negative seeds

--- src/hnccorr/seeds.py
+++ src/hnccorr/seeds.py
@@ -1,3 +1,4 @@
 class Seeds:
-    def __init__(self, pos_seeds):
+    def __init__(self, pos_seeds, neg_seeds):
         self.positive_seeds = pos_seeds
+        self.negative_seeds = neg_seeds
--- tests/test_seeds.py
+++ tests/test_seeds.py
@@ -2,11 +2,20 @@ import pytest
 
 
 @pytest.fixture
-def S(pos_seeds):
+def neg_seeds():
+    return {(5,), (6,)}
+
+
+@pytest.fixture
+def S(pos_seeds, neg_seeds):
     from hnccorr.seeds import Seeds
 
-    return Seeds(pos_seeds)
+    return Seeds(pos_seeds, neg_seeds)
 
 
 def test_positive_seeds(S, pos_seeds):
     assert S.positive_seeds == pos_seeds
+
+
+def test_negative_seeds(S, neg_seeds):
+    assert S.negative_seeds == neg_seeds
