83701959a2be1572dfe6ff09d4469ff700364f62
Quico Spaen <quico@spaen.nl>
2019-03-31 12:47:59 -0700

    Add positive seed logic to Seed class

--- src/hnccorr/seeds.py
+++ src/hnccorr/seeds.py
@@ -1,5 +1,26 @@
+from hnccorr.utils import eight_neighborhood, add_offset_set_coordinates
+
+
 class Seeds:
     def __init__(self, center_seed, pos_seeds, neg_seeds):
         self.center_seed = center_seed
         self.positive_seeds = pos_seeds
         self.negative_seeds = neg_seeds
+
+    def select_positive_seeds(self, radius, patch_size):
+        num_dimensions = len(patch_size)
+        offsets = eight_neighborhood(num_dimensions, radius - 1)
+        # compute positive seeds
+        positive_seeds = add_offset_set_coordinates(offsets, self.center_seed)
+        # check if seeds are within boundaries
+        positive_seeds = {
+            seed
+            for seed in positive_seeds
+            if all(
+                [
+                    x >= 0 and x < dim_len
+                    for x, dim_len in zip(seed, patch_size)
+                ]
+            )
+        }
+        return positive_seeds
--- tests/test_seeds.py
+++ tests/test_seeds.py
@@ -23,3 +23,34 @@ def test_positive_seeds(S, pos_seeds):
 
 def test_negative_seeds(S, neg_seeds):
     assert S.negative_seeds == neg_seeds
+
+
+@pytest.mark.parametrize(
+    "center_seed, radius, expected_seeds",
+    [
+        ((0, 0), 1, [(0, 0)]),
+        ((0, 0), 2, [(0, 0), (0, 1), (1, 1), (1, 0)]),
+        (
+            (1, 1),
+            2,
+            [
+                (0, 0),
+                (0, 1),
+                (0, 2),
+                (1, 0),
+                (1, 1),
+                (1, 2),
+                (2, 0),
+                (2, 1),
+                (2, 2),
+            ],
+        ),
+    ],
+)
+def test_select_positive_seeds(center_seed, radius, expected_seeds):
+    from hnccorr.seeds import Seeds
+
+    s = Seeds(center_seed, [], [])
+    seeds = s.select_positive_seeds(radius, (5, 5))
+
+    assert seeds == set(expected_seeds)
