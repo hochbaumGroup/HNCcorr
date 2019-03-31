e12e4069c2ba8065f1eece516e6e7cced67b1906
Quico Spaen <quico@spaen.nl>
2019-03-31 13:59:56 -0700

    Add negative seed selection method to seeds class

--- src/hnccorr/seeds.py
+++ src/hnccorr/seeds.py
@@ -1,4 +1,5 @@
 from hnccorr.utils import eight_neighborhood, add_offset_set_coordinates
+from math import sin, cos, pi
 
 
 class Seeds:
@@ -7,20 +8,37 @@ class Seeds:
         self.positive_seeds = pos_seeds
         self.negative_seeds = neg_seeds
 
-    def select_positive_seeds(self, radius, patch_size):
-        num_dimensions = len(patch_size)
+    def select_positive_seeds(self, radius, movie_size):
+        num_dimensions = len(movie_size)
         offsets = eight_neighborhood(num_dimensions, radius - 1)
         # compute positive seeds
         positive_seeds = add_offset_set_coordinates(offsets, self.center_seed)
         # check if seeds are within boundaries
-        positive_seeds = {
-            seed
-            for seed in positive_seeds
-            if all(
-                [
-                    x >= 0 and x < dim_len
-                    for x, dim_len in zip(seed, patch_size)
-                ]
+        return extract_valid_pixels(positive_seeds, movie_size)
+
+    def select_negative_seeds(self, radius, count, movie_size):
+        if len(movie_size) != 2:
+            raise ValueError(
+                "Only 2-dimensional movies are currently supported."
             )
+
+        angle_step = 2 * pi / float(count)
+
+        angles = [i * angle_step for i in range(count)]
+        offsets = {
+            (round(radius * cos(x)), round(radius * sin(x))) for x in angles
         }
-        return positive_seeds
+        negative_seeds = add_offset_set_coordinates(offsets, self.center_seed)
+
+        # check if seeds are within boundaries
+        return extract_valid_pixels(negative_seeds, movie_size)
+
+
+def extract_valid_pixels(pixel_set, region_size):
+    return {
+        pixel
+        for pixel in pixel_set
+        if all(
+            [x >= 0 and x < dim_len for x, dim_len in zip(pixel, region_size)]
+        )
+    }
--- tests/test_seeds.py
+++ tests/test_seeds.py
@@ -13,6 +13,13 @@ def S(pos_seeds, neg_seeds):
     return Seeds((5,), pos_seeds, neg_seeds)
 
 
+@pytest.fixture
+def empty_seed():
+    from hnccorr.seeds import Seeds
+
+    return Seeds(None, [], [])
+
+
 def test_center_seed(S):
     assert S.center_seed == (5,)
 
@@ -28,12 +35,12 @@ def test_negative_seeds(S, neg_seeds):
 @pytest.mark.parametrize(
     "center_seed, radius, expected_seeds",
     [
-        ((0, 0), 1, [(0, 0)]),
-        ((0, 0), 2, [(0, 0), (0, 1), (1, 1), (1, 0)]),
+        ((0, 0), 1, {(0, 0)}),
+        ((0, 0), 2, {(0, 0), (0, 1), (1, 1), (1, 0)}),
         (
             (1, 1),
             2,
-            [
+            {
                 (0, 0),
                 (0, 1),
                 (0, 2),
@@ -43,14 +50,59 @@ def test_negative_seeds(S, neg_seeds):
                 (2, 0),
                 (2, 1),
                 (2, 2),
-            ],
+            },
         ),
     ],
 )
-def test_select_positive_seeds(center_seed, radius, expected_seeds):
-    from hnccorr.seeds import Seeds
+def test_select_positive_seeds(
+    empty_seed, center_seed, radius, expected_seeds
+):
+    empty_seed.center_seed = center_seed
+    assert empty_seed.select_positive_seeds(radius, (5, 5)) == expected_seeds
 
-    s = Seeds(center_seed, [], [])
-    seeds = s.select_positive_seeds(radius, (5, 5))
 
-    assert seeds == set(expected_seeds)
+def test_select_negative_seeds_simple(empty_seed):
+    empty_seed.center_seed = (5, 5)
+    assert empty_seed.select_negative_seeds(2, 4, (10, 10)) == {
+        (3, 5),
+        (7, 5),
+        (5, 3),
+        (5, 7),
+    }
+
+
+def test_select_negative_seeds_zero_radius(empty_seed):
+    empty_seed.center_seed = (5, 5)
+    assert empty_seed.select_negative_seeds(0, 4, (10, 10)) == {(5, 5)}
+
+
+def test_select_negative_seeds_count_rounding(empty_seed):
+    empty_seed.center_seed = (5, 5)
+    assert empty_seed.select_negative_seeds(2, 8, (10, 10)) == {
+        (3, 5),
+        (4, 4),
+        (7, 5),
+        (4, 6),
+        (5, 3),
+        (6, 4),
+        (5, 7),
+        (6, 6),
+    }
+
+
+def test_select_negative_seeds_topleft_corner(empty_seed):
+    empty_seed.center_seed = (0, 0)
+    assert empty_seed.select_negative_seeds(2, 4, (10, 10)) == {(0, 2), (2, 0)}
+
+
+def test_select_negative_seeds_bottomright_corner(empty_seed):
+    empty_seed.center_seed = (9, 9)
+    assert empty_seed.select_negative_seeds(2, 4, (10, 10)) == {(7, 9), (9, 7)}
+
+
+def test_select_negative_seeds_invalid_dimension(empty_seed):
+    with pytest.raises(ValueError):
+        empty_seed.select_negative_seeds(2, 4, (10, 10, 10))
+
+    with pytest.raises(ValueError):
+        empty_seed.select_negative_seeds(2, 4, (10,))
