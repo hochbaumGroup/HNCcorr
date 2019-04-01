9e0270a389467b209403a55608aa5e59730fffbb
Quico Spaen <quico@spaen.nl>
2019-03-31 17:51:40 -0700

    Simplify postprocessor tests

--- tests/test_postprocessor.py
+++ tests/test_postprocessor.py
@@ -1,35 +1,28 @@
 import pytest
+from hnccorr.postprocessor import SizePostprocessor
+from hnccorr.segmentation import Segmentation
 
 
 @pytest.fixture
-def PP():
-    from hnccorr.postprocessor import SizePostprocessor
-
-    return SizePostprocessor(2, 10, 3)
+def postprocessor():
+    return SizePostprocessor(2, 5, 3)
 
 
-@pytest.fixture
-def S3(S):
-    return S(
-        {
-            (0, 0),
-            (1, 0),
-            (2, 0),
-            (0, 1),
-            (2, 1),
-            (0, 2),
-            (1, 2),
-            (2, 2),
-            (3, 0),
-            (3, 1),
-            (3, 2),
-        }
+def test_select_preference(postprocessor):
+    S1 = Segmentation({(0, 1), (0, 2), (0, 3)}, 0.5)
+    S2 = Segmentation({(0, 1), (0, 2)}, 0.5)
+    assert postprocessor.select([S1, S2]) == S1
+
+
+def test_select_no_valid_candidates(postprocessor):
+    assert (
+        postprocessor.select(
+            [
+                Segmentation({(0, 1)}, 0.5),
+                Segmentation(
+                    {(0, 0), (1, 0), (2, 0), (0, 1), (2, 1), (0, 2)}, 0.5
+                ),
+            ]
+        )
+        is None
     )
-
-
-def test_size_postprocessor_select(PP, S1, S2):
-    assert PP.select([S1, S2]) == S1
-
-
-def test_size_postprocessor_select_no_candidates(PP, S2, S3):
-    assert PP.select([S2, S3]) is None
