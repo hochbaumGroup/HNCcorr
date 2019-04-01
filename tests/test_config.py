983f34d2963f2fbc98451b169d36adff437fda4d
Quico Spaen <quico@spaen.nl>
2019-03-31 17:57:30 -0700

    Clarify config tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,34 +1,32 @@
 import pytest
 
+from hnccorr.config import Config
+
 
 @pytest.fixture
-def c1():
-    from hnccorr.config import Config
-
+def config():
     return Config(patch_size=31, negative_seed_radius=10)
 
 
-def test_config(c1):
-    assert c1.patch_size == 31
-    assert c1.negative_seed_radius == 10
+def test_attributes(config):
+    assert config.patch_size == 31
+    assert config.negative_seed_radius == 10
 
 
-def test_config_add(c1):
-    from hnccorr.config import Config
+def test_add_config(config):
+    config2 = Config(patch_size=21, positive_seed_size=5)
 
-    c2 = Config(patch_size=21, positive_seed_size=5)
-
-    c3 = c1 + c2
-    assert c1.patch_size == 31
-    assert c2.patch_size == 21
-    assert c3.patch_size == 21
+    config3 = config + config2
+    assert config.patch_size == 31
+    assert config2.patch_size == 21
+    assert config3.patch_size == 21
 
     with pytest.raises(AttributeError):
-        c2.negative_seed_radius
-    assert c1.negative_seed_radius == 10
-    assert c3.negative_seed_radius == 10
+        config2.negative_seed_radius
+    assert config.negative_seed_radius == 10
+    assert config3.negative_seed_radius == 10
 
     with pytest.raises(AttributeError):
-        c1.positive_seed_size == 5
-    assert c2.positive_seed_size == 5
-    assert c3.positive_seed_size == 5
+        config.positive_seed_size == 5
+    assert config2.positive_seed_size == 5
+    assert config3.positive_seed_size == 5
