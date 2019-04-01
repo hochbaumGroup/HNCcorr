c4139dccf0fa056d13bd4c7ef0d5d0f2809ae04c
Quico Spaen <quico@spaen.nl>
2019-03-31 17:54:53 -0700

    Remove redundant fixture in seeder test

--- tests/test_seeder.py
+++ tests/test_seeder.py
@@ -1,14 +1,10 @@
 import pytest
 
-
-@pytest.fixture
-def LCS(MM):
-    from hnccorr.seeder import LocalCorrelationSeeder
-
-    return LocalCorrelationSeeder(MM, neighborhood_size=3, keep_fraction=0.2)
+from hnccorr.seeder import LocalCorrelationSeeder
 
 
-def test_local_corr_seeder(LCS):
+def test_local_corr_seeder(MM):
+    LCS = LocalCorrelationSeeder(MM, neighborhood_size=3, keep_fraction=0.2)
     assert LCS.next() == (9,)
     assert LCS.next() == (8,)
     assert LCS.next() is None
