c85367d1a3508b0f08d70c935c08e7ea87b04ca8
Quico Spaen <quico@spaen.nl>
2019-05-29 13:47:26 -0700

    Add best segmentation property to candidate

--- src/hnccorr/candidate.py
+++ src/hnccorr/candidate.py
@@ -5,6 +5,7 @@ class Candidate:
     def __init__(self, value):
         self._value = value
         self.segmentations = None
+        self.best_segmentation = None
 
     def __eq__(self, other):
         return self._value == other._value
@@ -12,5 +13,5 @@ class Candidate:
     def segment(self):
         self.segmentations = [Segmentation({(0, 1)}, 1.0)]
 
-        best_segmentation = self.segmentations[0]
-        return best_segmentation
+        self.best_segmentation = self.segmentations[0]
+        return self.best_segmentation
--- tests/test_candidate.py
+++ tests/test_candidate.py
@@ -13,5 +13,12 @@ def test_candidate_segment(simple_candidate, simple_segmentation):
 
 
 def test_candidate_segmentations(simple_candidate, simple_segmentation):
+    assert simple_candidate.segmentations is None
     simple_candidate.segment()
     assert simple_candidate.segmentations == [simple_segmentation]
+
+
+def test_candidate_best_segmentations(simple_candidate, simple_segmentation):
+    assert simple_candidate.best_segmentation is None
+    simple_candidate.segment()
+    assert simple_candidate.best_segmentation == simple_segmentation
