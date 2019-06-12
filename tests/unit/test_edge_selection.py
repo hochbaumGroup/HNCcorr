6f6b5a36c6f6c6c9ded2029da0fb291fd2be3aa1
Quico Spaen <quico@spaen.nl>
2019-06-12 16:24:16 -0700

    introduce mock_embedding fixture

--- tests/unit/test_edge_selection.py
+++ tests/unit/test_edge_selection.py
@@ -5,26 +5,29 @@ from hnccorr.edge_selection import SparseComputation
 
 
 @pytest.fixture
-def embedding():
-    class MockEmbedding:
-        def __init__(self):
-            self.embedding = np.array([[-1, 0], [-0.9, 0], [0, 0], [0.9, 0], [1, 0]]).T
-
-    return MockEmbedding()
+def mock_embedding(mocker, dummy):
+    return mocker.patch("hnccorr.embedding.CorrelationEmbedding", autospec=True)(dummy)
 
 
-def test_sparse_computation(embedding):
-    assert SparseComputation(2, 0.2).select_edges(embedding) == {
+def test_sparse_computation(mock_embedding):
+    mock_embedding.embedding = np.array(
+        [[-1, 0], [-0.9, 0], [0, 0], [0.9, 0], [1, 0]]
+    ).T
+    assert SparseComputation(2, 0.2).select_edges(mock_embedding) == {
         ((0,), (1,)),
         ((3,), (4,)),
     }
 
 
-def test_sparse_computation_with_dimension_reducer(embedding):
+def test_sparse_computation_with_dimension_reducer(mock_embedding):
+    mock_embedding.embedding = np.array(
+        [[-1, 0], [-0.9, 0], [0, 0], [0.9, 0], [1, 0]]
+    ).T
+
     class MockDimReducer:
         def fit_transform(self, data, **kwargs):
             return data
 
     assert SparseComputation(2, 0.2, dimension_reducer=MockDimReducer()).select_edges(
-        embedding
+        mock_embedding
     ) == {((0,), (1,)), ((3,), (4,))}
