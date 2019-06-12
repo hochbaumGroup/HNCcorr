f97716004dec1a013d997b118ce3b68390d5d161
Quico Spaen <quico@spaen.nl>
2019-06-12 16:33:23 -0700

    Combine a bunch of test cases

--- tests/unit/test_seeds.py
+++ tests/unit/test_seeds.py
@@ -19,38 +19,27 @@ def test_positive_seed_selector(center_seed, radius, expected_seeds):
     assert PositiveSeedSelector(radius).select(center_seed, (5, 5)) == expected_seeds
 
 
-def test_select_negative_seed_selector():
-    assert NegativeSeedSelector(2, 4).select((5, 5), (10, 10)) == {
-        (3, 5),
-        (7, 5),
-        (5, 3),
-        (5, 7),
-    }
-
-
-def test_select_negative_seeds_zero_radius():
-    assert NegativeSeedSelector(0, 8).select((5, 5), (10, 10)) == {(5, 5)}
-
-
-def test_select_negative_seeds_count_rounding():
-    assert NegativeSeedSelector(2, 8).select((5, 5), (10, 10)) == {
-        (3, 5),
-        (4, 4),
-        (7, 5),
-        (4, 6),
-        (5, 3),
-        (6, 4),
-        (5, 7),
-        (6, 6),
-    }
-
-
-def test_select_negative_seeds_topleft_corner():
-    assert NegativeSeedSelector(2, 4).select((0, 0), (10, 10)) == {(0, 2), (2, 0)}
-
-
-def test_select_negative_seeds_bottomright_corner():
-    assert NegativeSeedSelector(2, 4).select((9, 9), (10, 10)) == {(7, 9), (9, 7)}
+@pytest.mark.parametrize(
+    "radius, count, center_seed, movie_size, expected_seeds",
+    [
+        (0, 8, (5, 5), (10, 10), {(5, 5)}),
+        (2, 4, (0, 0), (10, 10), {(0, 2), (2, 0)}),
+        (2, 4, (9, 9), (10, 10), {(9, 7), (7, 9)}),
+        (
+            2,
+            8,
+            (5, 5),
+            (10, 10),
+            {(3, 5), (4, 4), (7, 5), (4, 6), (5, 3), (6, 4), (5, 7), (6, 6)},
+        ),
+        (2, 4, (5, 5), (10, 10), {(3, 5), (7, 5), (5, 3), (5, 7)}),
+    ],
+)
+def test_select_negative_seeds(radius, count, center_seed, movie_size, expected_seeds):
+    assert (
+        NegativeSeedSelector(radius, count).select(center_seed, movie_size)
+        == expected_seeds
+    )
 
 
 def test_select_negative_seeds_invalid_dimension():
