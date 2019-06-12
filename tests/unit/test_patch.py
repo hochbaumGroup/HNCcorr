1f5254553a72fca919e2d0c287783737050cca67
Quico Spaen <quico@spaen.nl>
2019-06-12 16:45:57 -0700

    Simplify patch tests

--- tests/unit/test_patch.py
+++ tests/unit/test_patch.py
@@ -6,25 +6,30 @@ from pytest_mock import mocker
 from hnccorr.patch import Patch
 
 
-def test_even_windowsize(MM, pos_seeds):
+@pytest.fixture
+def simple_patch(MM):
+    return Patch(MM, (9,), 7)
+
+
+def test_even_windowsize(MM):
     with pytest.raises(ValueError):
         Patch(MM, (5,), 6)
 
 
-def test_pixel_size(P1):
-    assert P1.pixel_size == (7,)
+def test_pixel_size(simple_patch):
+    assert simple_patch.pixel_size == (7,)
 
 
-def test_num_frames(P1):
-    assert P1.num_frames == 3
+def test_num_frames(simple_patch):
+    assert simple_patch.num_frames == 3
 
 
-def test_data(P1, MM):
-    np.testing.assert_equal(P1[:], MM[:, 2:9])
+def test_data(simple_patch, MM):
+    np.testing.assert_equal(simple_patch[:], MM[:, 3:10])
 
 
-def test_patch_enumerate_pixels(MM, MM2):
-    assert Patch(MM, (5,), 3).enumerate_pixels() == {(4,), (5,), (6,)}
+def test_patch_enumerate_pixels(simple_patch, MM2):
+    assert simple_patch.enumerate_pixels() == {(3,), (4,), (5,), (6,), (7,), (8,), (9,)}
     assert Patch(MM2, (5, 5), 3).enumerate_pixels() == {
         (4, 4),
         (4, 5),
@@ -38,13 +43,11 @@ def test_patch_enumerate_pixels(MM, MM2):
     }
 
 
-def test_to_movie_index(P):
-    patch = P((9,))
-    patch.to_movie_index((3,)) == (6,)
-    patch.to_movie_index((0,)) == (3,)
+def test_to_movie_index(simple_patch, MM):
+    simple_patch.to_movie_index((3,)) == (6,)
+    simple_patch.to_movie_index((0,)) == (3,)
 
 
-def test_to_patch_index(P):
-    patch = P((9,))
-    patch.to_patch_index((9,)) == (6,)
-    patch.to_patch_index((3,)) == (0,)
+def test_to_patch_index(simple_patch, MM):
+    simple_patch.to_patch_index((9,)) == (6,)
+    simple_patch.to_patch_index((3,)) == (0,)
