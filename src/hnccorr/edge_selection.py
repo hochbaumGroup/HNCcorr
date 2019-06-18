aceb7b35fe5812e793f79b7eca7b3556ce0eb1b8
Quico Spaen <quico@spaen.nl>
2019-06-18 12:48:37 -0700

    Documentation for sparsecomputation wrapper

--- src/hnccorr/edge_selection.py
+++ src/hnccorr/edge_selection.py
@@ -4,7 +4,27 @@ from sparsecomputation import ApproximatePCA
 
 
 class SparseComputationEmbeddingWrapper:
+    """Wrapper for SparseComputation that accepts an embedding.
+
+    Attributes:
+        _sc (SparseComputation): SparseComputation object.
+    """
+
     def __init__(self, dim_low, distance, dimension_reducer=None):
+        """Initializes a SparseComputationEmbeddingWrapper instance.
+
+        Args:
+            dim_low (int): Dimension of the low-dimensional space in sparse computation.
+            distance (float): 1 / grid_resolution. Defines the size of the grid blocks
+                in sparse computation.
+            dimension_reducer (DimReducer): Provides dimension reduction for sparse
+                computation. By default, approximate principle component analysis is
+                used.
+
+        Returns:
+            SparseComputationEmbeddingWrapper
+
+        """
         self._dim_low = int(dim_low)
         self._distance = distance
 
@@ -16,6 +36,18 @@ class SparseComputationEmbeddingWrapper:
         self._sc = SC(self._dim_reducer, distance=self._distance)
 
     def select_edges(self, embedding):
+        """Selects relevant pairwise similarities with sparse computation.
+
+        Determines the set of relevant pairwise similarities based on the sparse
+        computation algorithm. See sparse computation for details. Pixel coordinates
+        are with respect to the index of the embedding.
+
+        Args:
+            embedding (CorrelationEmbedding): Embedding of pixels into feature vectors.
+
+        Returns:
+            list(tuple): List of relevant pixel pairs.
+        """
         shape = embedding.embedding.shape[1:]
         data = embedding.embedding.reshape(-1, np.product(shape)).T
 
