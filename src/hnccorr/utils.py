f5281290e51b2af7e5a3d0fe4820c78608d7ce19
Quico Spaen <quico@spaen.nl>
2019-06-19 14:56:04 -0700

    document utilities

--- src/hnccorr/utils.py
+++ src/hnccorr/utils.py
@@ -4,8 +4,23 @@ from itertools import product
 
 
 def add_offset_set_coordinates(iterable, offset):
-    """Add offset to each coordinate in set"""
-    return set(add_offset_coordinates(c, offset) for c in iterable)
+    """Adds a fixed offset to all pixel coordinates in a set.
+
+    Args:
+        coordinates (set): Set of pixel coordinates. Each pixel coordinate is a tuple.
+        offset (tuple): Offset to add to each pixel coordinate. Tuple should be of the
+            same length as the tuples in `coordinates`.
+
+    Returns:
+        set: Set of updated coordinates.
+
+    Example:
+        .. code-block:: python
+
+            >>> add_offset_set_coordinates({(5, 2), (4, 7)}, (2, 2))
+            {(7, 4), (6, 9)}
+    """
+    return set(add_offset_to_coordinate(c, offset) for c in iterable)
 
 
 def add_offset_to_coordinate(coordinate, offset):
@@ -25,15 +40,34 @@ def add_offset_to_coordinate(coordinate, offset):
 
 
 def add_time_index(index):
-    """Add full slice to from time dimension to pixel index"""
+    """Inserts a full slice as the first dimension of an index for e.g. numpy.
+
+    Args:
+        index (tuple): Index for e.g. numpy array.
+
+    Returns:
+        tuple: New index with additional dimension.
+
+    Example:
+        .. code-block:: python
+
+            >>> add_time_index((5, :3))
+            (:, 5, :3)
+    """
     return (slice(None, None),) + index
 
 
 def list_images(folder):
-    """List and sort tiff images
+    """Lists and sorts tiff images in a folder.
+
+    Images are sorted in ascending order based on filename.
+
+    Caution:
+        Filenames are sorted as strings. Note that ``200.tiff`` is sorted before
+        ``5.tiff``. Pad image filenames with zeros to prevent this: ``005.tiff``.
 
     Args:
-        folder: folder containing tif(f) files.
+        folder: folder containing tiff image files.
 
     Returns:
         list: Sorted list of paths of tiff files in folder.
@@ -43,6 +77,25 @@ def list_images(folder):
 
 
 def four_neighborhood(num_dims):
+    """Returns all neighboring pixels of zero that differ in at most one coordinate.
+
+    Includes zero coordinate itself.
+
+    Args:
+        num_dims (int): Number of dimensions for the coordinates.
+
+    Returns:
+        set: Set of pixel coordinates.
+
+    Example:
+        .. code-block:: python
+
+            >>> four_neighborhood(1)
+            [(-1,), (0,), (1,)]
+            >>> eight_neighborhood(2)
+            [(-1, 0), (0, -1), (0, 0), (0, 1), (1, 0)]
+
+    """
     neighbors = []
     for dim, change in product(range(num_dims), (-1, 0, 1)):
         shift = [0] * num_dims
@@ -52,8 +105,42 @@ def four_neighborhood(num_dims):
 
 
 def eight_neighborhood(num_dims, max_radius):
+    """Returns all coordinates within a given L-infinity distance of zero.
+
+    Includes zero coordinate itself.
+
+    Args:
+        num_dims (int): Number of dimensions for the coordinates.
+        max_radius (int): Largest L-infinity distance allowed.
+
+    Returns:
+        set: Set of pixel coordinates.
+
+    Example:
+        .. code-block:: python
+
+            >>> eight_neighborhood(1, 1)
+            [(-1,), (0,), (1,)]
+            >>> eight_neighborhood(2, 1)
+            [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1, 1)]
+
+    """
     return set(product(range(-max_radius, max_radius + 1), repeat=num_dims))
 
 
 def generate_pixels(shape):
+    """Enumerate all pixel coordinates for a movie/patch.
+
+    Args:
+        shape (tuple): Shape of movie. Number of pixels in each dimension.
+
+    Returns:
+        Iterator: Iterates over all pixels.
+
+    Example:
+        .. code-block:: python
+
+            >>> generate_pixels((2,2))
+            [(0, 0), (0, 1), (1, 0), (1, 1)]
+    """
     return product(*[range(n) for n in shape])
