0b365061ccdde885e11207c666955990b4365467
Quico Spaen <quico@spaen.nl>
2019-06-25 16:19:35 -0700

    But use memmap for test

--- tests/unit/test_example.py
+++ tests/unit/test_example.py
@@ -4,5 +4,5 @@ from hnccorr.example import load_example_data
 
 
 def test_example_data_loader():
-    data = load_example_data(filedir=TEST_DATA_DIR)
+    data = load_example_data(filedir=TEST_DATA_DIR, memmap=True)
     assert data.shape == (800, 512, 512)
