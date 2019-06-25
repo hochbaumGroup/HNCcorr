2b5ed7e19421e7a68bfa4c58cfe381a704d48252
Quico Spaen <quico@spaen.nl>
2019-06-24 17:21:15 -0700

    Add copyright notices

--- LICENSE.md
+++ LICENSE.md
@@ -1,4 +1,4 @@
-Copyright © 2017. Quico Spaen, Roberto Asín-Achá, and the Regents of the University of California (Regents). All Rights Reserved.
+Copyright © 2017. Regents of the University of California (Regents). All Rights Reserved.
 
 Permission to use, copy, modify, and distribute this software and its documentation for educational, research, and not-for-profit purposes, without fee and without a signed licensing agreement, is hereby granted, provided that the above copyright notice, this paragraph and the following two paragraphs appear in all copies, modifications, and distributions. Contact The Office of Technology Licensing, UC Berkeley, 2150 Shattuck Avenue, Suite 510, Berkeley, CA 94720-1620, (510) 643-7201, for commercial licensing opportunities. Created by Quico Spaen, Roberto Asín-Achá, and Dorit S. Hochbaum, Department of Industrial Engineering and Operations Research, University of California, Berkeley.
 
--- setup.py
+++ setup.py
@@ -1,36 +1,60 @@
+# Copyright © 2017. Regents of the University of California (Regents). All Rights
+# Reserved.
+#
+# Permission to use, copy, modify, and distribute this software and its documentation
+# for educational, research, and not-for-profit purposes, without fee and without a
+# signed licensing agreement, is hereby granted, provided that the above copyright
+# notice, this paragraph and the following two paragraphs appear in all copies,
+# modifications, and distributions. Contact The Office of Technology Licensing, UC
+# Berkeley, 2150 Shattuck Avenue, Suite 510, Berkeley, CA 94720-1620, (510) 643-7201,
+# for commercial licensing opportunities. Created by Quico Spaen, Roberto Asín-Achá,
+# and Dorit S. Hochbaum, Department of Industrial Engineering and Operations Research,
+# University of California, Berkeley.
+#
+# IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL,
+# INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF THE USE
+# OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF REGENTS HAS BEEN ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
+# SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED "AS
+# IS". REGENTS HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+# ENHANCEMENTS, OR MODIFICATIONS.
 from setuptools import setup
 from setuptools import find_packages
 import unittest
 
 
 def readme():
-    with open('README.md') as f:
+    with open("README.md") as f:
         return f.read()
 
 
 def license():
-    with open('LICENSE.md') as f:
+    with open("LICENSE.md") as f:
         return f.read()
 
 
-setup(name='HNCcorr',
-      version='0.1dev',
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.5',
-      ],
-      description=None,
-      keywords=['Cell detection', 'Neuroscience', 'Calcium imaging'],
-      url='https://github.com/quic0/HNCcorr-python',
-      author='Quico Spaen',
-      author_email='qspaen@berkeley.edu',
-      license=license(),
-      long_description=readme(),
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      install_requires=['numpy', 'pillow'],  # required packages here
-      setup_requires=['pytest-runner', ],
-      tests_require=['pytest', 'mock'],
-      zip_safe=False
-      )
+setup(
+    name="HNCcorr",
+    version="0.1dev",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+    ],
+    description=None,
+    keywords=["Cell detection", "Neuroscience", "Calcium imaging"],
+    url="https://github.com/quic0/HNCcorr-python",
+    author="Quico Spaen",
+    author_email="qspaen@berkeley.edu",
+    license=license(),
+    long_description=readme(),
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=["numpy", "pillow"],  # required packages here
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest", "mock"],
+    zip_safe=False,
+)
