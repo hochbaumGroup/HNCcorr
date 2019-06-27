4d8eb6c96b7da2a729a2fd654b45af9b51036fcb
Quico Spaen <quico@spaen.nl>
2019-06-27 11:26:42 -0700

    Mock numpy import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,6 +37,9 @@ extensions = [
     "sphinx.ext.mathjax",
 ]
 
+# Mock imports for doc build
+autodoc_mock_imports = ["numpy"]
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
